0db32918a2feb36ef30823802c40dc419ef24302
arvindkumar422 <thiagarajan.ar@husky.neu.edu>
2019-03-27 20:12:47 -0400

    change url

--- src/main/java/com/example/Assignment1/services/TopicService.java
+++ src/main/java/com/example/Assignment1/services/TopicService.java
@@ -1,6 +1,5 @@
 package com.example.Assignment1.services;
 
-import com.example.Assignment1.model.Lesson;
 import com.example.Assignment1.model.Topic;
 import com.example.Assignment1.model.Widget;
 import com.example.Assignment1.repositories.LessonRepository;
